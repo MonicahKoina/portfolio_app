4b4e5e1742623a70054bfdf2625bb04843fef0cb
Monicah Koina <monicahkoina1@gmail.com>
2024-12-09 12:52:22 +0300

    added about view and removed the debug banner

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,45 +5,86 @@ class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   return const Scaffold(
+   return Scaffold(
      body: Column(
        children: [
-         Padding(
+         const Padding(
            padding: EdgeInsets.only( top: 100.0),
-           child: Text("Frontend Developer"),
+           child: Text("Frontend Developer",
+           style: TextStyle(
+               fontSize: 30,
+
+           ),),
+
          ),
-         SizedBox(height: 30,),
-         CircleAvatar(radius: 80,),
-         SizedBox(height: 30),
-         Text("Monicah Koina"),
-         SizedBox(height: 30),
-         Column(
-           children: [
-             Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Icon(Icons.phone ),
-                 Text("+254712345678")
-               ],
-             ),
-             Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Icon(Icons.email),
-                 Text("monicahkoina1@gmail.com")
-               ],
-             ),
-             Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Icon(Icons.location_city),
-                 Text("Nairobi, Kenya")
-               ],
-             )
-           ],
+         const SizedBox(height: 30,),
+         const CircleAvatar(radius: 80,
+         backgroundImage: AssetImage('assets/profpic.jpeg'),
+         ),
+         const SizedBox(height: 30),
+         const Text("Nyamon Developers",
+         style: TextStyle(fontSize: 20),),
+         const SizedBox(height: 30),
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Column(
+             children: [
+               const Row(
+                 children: [
+                   Icon(Icons.house),
+                   SizedBox(width: 10),
+                   Text("Full time",
+                   style: TextStyle(fontSize: 20),
+                   )
+                 ],
+               ),
+               const SizedBox(height: 10),
+               const Row(
+
+                 children: [
+                   Icon(Icons.phone ),
+                   SizedBox(width: 10),
+                   Text("+254712345678",
+                   style: TextStyle(fontSize: 20),)
+                 ],
+               ),
+               const SizedBox(height: 10),
+               const Row(
+
+                 children: [
+                   Icon(Icons.email),
+                   SizedBox(width: 10),
+                   Text("monicahkoina1@gmail.com",
+                   style: TextStyle(fontSize: 20),)
+                 ],
+               ),
+               const SizedBox(height: 10),
+               const Row(
+
+                 children: [
+                   Icon(Icons.location_city),
+                   SizedBox(width: 10),
+                   Text("Nairobi, Kenya",
+                   style: TextStyle(fontSize: 20),)
+                 ],
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(30.0),
+                 child: SizedBox(
+                   width: 300,
+                   height: 40,
+                   child: FloatingActionButton(
+                       hoverColor: Colors.amberAccent,
+                       onPressed: (){
+                   },child: const Text("About me")),
+                 ),
+               ),
+             ],
+           ),
          )
        ],
      ),
+
    );
   }
 
